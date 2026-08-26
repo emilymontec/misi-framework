9faaece471efb8798d444123c788c660f108485d
emilymontec <emilymonterrosa.c@gmail.com>
2026-08-25 23:22:05 -0500

    Correccion general

--- examples/demo-app/modules/Reports/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Modules\Reports\Controllers\SummaryController;
-
-/** @var \Misi\Routing\Router $router */
-
-$router->get('/modules/reports/summary', [SummaryController::class, '__invoke'], ['auth']);
